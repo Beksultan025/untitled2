131742649742640303faeee8942715191d39cedd
Beksultan025 <ukuevb08@gmail.com>
2021-12-22 22:01:53 +0600

    Initial commit

---
+++ src/com/company/Magic.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Magic extends Hero{
+    @Override
+    public String applySuperAbility(String superAbilityType) {
+        System.out.println("Warrior применил суперспособность CRITICAL DAMAGE");
+        return null;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HavingSuperAbility[] dads = {new Medic() , new Warrior() , new Magic()};
+        for (int i = 0; i < dads.length; i++) {
+        }
+    }
+}
