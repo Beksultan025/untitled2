d2c196cc1ca6281f699e99655dc10648725ce6ed
Beksultan025 <ukuevb08@gmail.com>
2021-12-22 22:24:39 +0600

    lesson7.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         HavingSuperAbility[] dads = {new Medic() , new Warrior() , new Magic()};
         for (int i = 0; i < dads.length; i++) {
-            dads[i].applySuperAbility("");
+            dads[i].applySuperAbility(" ");
         }
     }
 }
